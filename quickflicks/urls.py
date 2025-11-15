b7490c0b62011bd22f1902f65a4a5797c4d08c39
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-15 14:35:43 +0000

    First test view for heroku deployment

--- quickflicks/urls.py
+++ quickflicks/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+# test home page for heroku deployment
+def home(request):
+    return HttpResponse("QuickFlicks: Deployment test successful.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
