750a97f73d9e3c980c4085cac9858c7a1a7eba5b
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-17 17:48:33 +0000

    change logout path in accounts/urls.py to redirect to home page and not default admin/logout page

--- accounts/urls.py
+++ accounts/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     # AUTHENTICATION (Custom Login / Django Logout)
     # ---------------------------------------------------------
     path("login/", views.login_view, name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 
     # ---------------------------------------------------------
     # SIGNUP (Custom)
