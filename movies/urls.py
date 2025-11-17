c3dd8f830018f154e2b8e36e9fd2f3531840c015
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-17 18:00:16 +0000

    Add URL routes for shelf actions

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,33 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+
+    # User shelf
     path("my-shelf/", views.my_shelf, name="my_shelf"),
+
+    # Change movie status (to_watch, watched, to_put_away)
+    path(
+        "change-status/<int:movie_id>/<str:new_status>/",
+        views.change_status,
+        name="change_status"
+    ),
+
+    # Remove movie from shelf
+    path(
+        "remove-movie/<int:movie_id>/",
+        views.remove_movie,
+        name="remove_movie"
+    ),
+
+    # Rating (thumb up / down)
+    path(
+        "thumb-up/<int:movie_id>/",
+        views.thumb_up,
+        name="thumb_up"
+    ),
+    path(
+        "thumb-down/<int:movie_id>/",
+        views.thumb_down,
+        name="thumb_down"
+    ),
 ]
